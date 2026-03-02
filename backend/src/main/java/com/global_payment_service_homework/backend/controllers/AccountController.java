21ec5e38f8bda973a65eb3f9bff95e2c2f31b35c
Tamás Varga <varga.tamas.pro@gmail.com>
2026-03-02 10:02:14 +0100

    feat: add api to the path of all accounts endpoits

--- backend/src/main/java/com/global_payment_service_homework/backend/controllers/AccountController.java
+++ backend/src/main/java/com/global_payment_service_homework/backend/controllers/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
@@ -13,6 +14,7 @@ import com.global_payment_service_homework.backend.models.Account;
 import com.global_payment_service_homework.backend.services.AccountService;
 
 @RestController
+@RequestMapping(value = "/api")
 public class AccountController {
 
     private final AccountService accountService;
